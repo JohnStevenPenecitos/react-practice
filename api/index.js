37299d0f3fca23cc6db529c80710e3026445e0f9
JohnStevenPenecitos <steven.penecitos123@gmail.com>
2024-03-02 17:00:38 +0800

    change the cors origin to client url https

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["my-react-app-sandy-six.vercel.app"],
+    origin: ["https://my-react-app-sandy-six.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
   }
