79cb0a371070c3eed91cc9dd0215df4fc61ae4be
JohnStevenPenecitos <steven.penecitos123@gmail.com>
2024-03-25 19:55:59 +0800

    modify index5

--- api/index.js
+++ api/index.js
@@ -19,8 +19,8 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: ["https://my-react-app-sandy-six.vercel.app"],
-    origin: ["http://192.168.100.63:5173"],
-    // origin: ["https://react-practice-uk4m.onrender.com/"],
+    // origin: ["http://192.168.100.63:5173"],
+    origin: ["https://react-practice-uk4m.onrender.com/"],
 
     // origin: ["http://localhost:5173"],
     methods: ["POST", "GET", "PUT", "DELETE"],
@@ -50,8 +50,8 @@ const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     // origin: "http://localhost:5173", // Replace with your client's origin
-    origin: "http://192.168.100.63:5173", // Replace with your client's origin
-    // origin: "https://react-practice-uk4m.onrender.com/", // Replace with your client's origin
+    // origin: "http://192.168.100.63:5173", // Replace with your client's origin
+    origin: "https://react-practice-uk4m.onrender.com/", // Replace with your client's origin
 
     methods: ["GET", "POST"],
   },
